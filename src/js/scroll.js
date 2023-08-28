36ddf613b45832e7e4c94fe95b73baf56319a614
nahirniy <nahirniyy@gmail.com>
2023-08-28 22:37:33 +0300

    update scroll and style for footer

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,7 +1,20 @@
 const links = document.querySelectorAll('a[href^="#"]');
+const scrollToTopButton = document.getElementById('scrollToTop');
+const body = document.body;
+const html = document.documentElement;
 
 links.forEach(link => {
   link.addEventListener('click', smoothScroll);
+});
+
+scrollToTopButton.addEventListener('click', scrollToTop);
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
 });
 
 function smoothScroll(e) {
@@ -10,10 +23,24 @@ function smoothScroll(e) {
   const targetId = this.getAttribute('href');
   const targetElement = document.querySelector(targetId);
 
+  if (targetElement) {
+    const offsetTop =
+      targetElement.getBoundingClientRect().top + window.scrollY;
+
+    anime({
+      targets: [body, html],
+      scrollTop: offsetTop,
+      duration: 1000,
+      easing: 'easeInOutQuad',
+    });
+  }
+}
+
+function scrollToTop() {
   anime({
-    targets: document.documentElement,
-    scrollTop: targetElement.offsetTop,
-    duration: 1750,
+    targets: [body, html],
+    scrollTop: 0,
+    duration: 1000,
     easing: 'easeInOutQuad',
   });
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,2 @@
 import './js/mobile-menu.js';
 import './js/scroll';
-import './js/scrollup';
